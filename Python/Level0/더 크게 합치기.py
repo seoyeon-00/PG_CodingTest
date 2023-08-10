a9324bc3e9d5aa100bed2479320140f0e63c3109
ksy <ksoup3434@gmail.com>
2023-08-10 23:47:09 +0900

    [add] 230810 풀이

--- Python/Level0/더 크게 합치기.py
+++ Python/Level0/더 크게 합치기.py
@@ -5,7 +5,6 @@
 # 양의 정수 a와 b가 주어졌을 때, a ⊕ b와 b ⊕ a 중 더 큰 값을 return 하는 solution 함수를 완성해 주세요.
 # 단, a ⊕ b와 b ⊕ a가 같다면 a ⊕ b를 return 합니다.
 
-
 def solution(a, b):
     sum1 = str(a) + str(b)
     sum2 = str(b) + str(a)
