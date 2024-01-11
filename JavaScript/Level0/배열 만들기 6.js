50353aef2bbe28a5fe490a37c976efb2ce48bbdf
ksy <ksoup3434@gmail.com>
2024-01-11 23:30:24 +0900

    [add] 240111 풀이

---
+++ JavaScript/Level0/배열 만들기 6.js
@@ -0,0 +1,42 @@
+/*
+[문제]
+0과 1로만 이루어진 정수 배열 arr가 주어집니다. arr를 이용해 새로운 배열 stk을 만드려고 합니다.
+
+i의 초기값을 0으로 설정하고 i가 arr의 길이보다 작으면 다음을 반복합니다.
+
+만약 stk이 빈 배열이라면 arr[i]를 stk에 추가하고 i에 1을 더합니다.
+stk에 원소가 있고, stk의 마지막 원소가 arr[i]와 같으면 stk의 마지막 원소를 stk에서 제거하고 i에 1을 더합니다.
+stk에 원소가 있는데 stk의 마지막 원소가 arr[i]와 다르면 stk의 맨 마지막에 arr[i]를 추가하고 i에 1을 더합니다.
+위 작업을 마친 후 만들어진 stk을 return 하는 solution 함수를 완성해 주세요.
+
+단, 만약 빈 배열을 return 해야한다면 [-1]을 return 합니다.
+
+[제한사항]
+- 1 ≤ arr의 길이 ≤ 1,000,000
+- arr의 원소는 0 또는 1 입니다.
+
+[입출력]
+|         arr        |     result      |
+|--------------------|-----------------|
+|   [0, 1, 1, 1, 0]	 |    [0, 1, 0]    |
+|   [0, 1, 0, 1, 0]  | [0, 1, 0, 1, 0] |
+|    [0, 1, 1, 0]    |       [-1]      |
+*/
+
+function solution(arr) {
+  // 처음에는 빈배열로 시작하므로, arr[0]을 answer에 추가 후,
+  // 반복문은 index 1부터 시작
+  var answer = [arr[0]];
+
+  // arr 길이만큼 반복문 실행
+  for (let i = 1; i < arr.length; i++) {
+    if (answer[answer.length - 1] === arr[i]) {
+      answer.pop(); // 마지막 원소 삭제
+    } else {
+      answer.push(arr[i]); // arr[i] 원소 추가
+    }
+  }
+
+  // 빈배열이면 [-1]을 반환
+  return answer.length === 0 ? [-1] : answer;
+}
